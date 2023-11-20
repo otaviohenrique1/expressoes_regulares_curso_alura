6af95c0088170f9a7b678c189d9d8220203e601e
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-11-20 10:42:21 -0300

    commit 2

--- buscaTelefone.js
+++ buscaTelefone.js
@@ -3,7 +3,7 @@ const bancoCsv = "database.csv";
 const banco = fs.readFileSync(bancoCsv, "utf-8");
 
 // const regexTelefone = /\(\d+\)\s\d+-\d+/g;
-const regexTelefone = /\(\d+\)\s\d+-\d+/g;
+const regexTelefone = /\(\d{2}\)\s\d{4,5}-\d{4}/g;
 
 const matchTelefone = banco.match(regexTelefone);
 console.log(matchTelefone);
